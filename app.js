dbb7263221cd9be7c8b961cf30051474db796052
Vishal Reddy <59984658+Vishalreddy114@users.noreply.github.com>
2020-05-01 13:37:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const dotenv = require('dotenv');
+var compression = require('compression');
+var helmet = require('helmet');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
-const app = express();
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://dwstudent:Vishal9966@cluster0-cfre7.mongodb.net/test?retryWrites=true&w=majority';
+
+var app = express();
+dotenv.config({ path: '.env' })
+
+var mongoose = require('mongoose');
+var dev_db_url = process.env.ATLAS_URI;
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,11 +32,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/catalog', catalogRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
